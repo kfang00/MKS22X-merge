8a9f9ba177eec234f34bef21ece85fe962f39139
Kayla Fang <kfang00@stuy.edu>
2019-03-15 11:02:05 -0400

    basic format from website

---
+++ Merge.java
@@ -0,0 +1,15 @@
+public class Merge {
+  /*
+  mergesort(data,lo,hi):
+    if lo >= hi :
+      return
+    mergesort left side
+    mergesort right side
+    merge*/
+
+
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+
+  }
+}
